e78dc0a726e5b797e162a1aa2a0988d6ab2a4e18
Russel-hunho <bbhh1000@gmail.com>
2023-04-24 23:02:35 +0900

    23.04.24

--- Programmers_SQL/공부노트.sql
+++ Programmers_SQL/공부노트.sql
@@ -7,4 +7,24 @@
 
 # HOUR()
  -> 인트형으로 되는듯!
- 
+
+LIMIT N;
+-- ORDER BY 이후 상위 N개만 출력
+-- 모든 구문의 맨 마지막!
+
+
+-- 구문 순서
+SELECT A, COUNT(*), AVG(B+C) AS D
+FROM AA
+
+JOIN BB AS C
+ON AA.A = C.D
+
+WHERE AA.A > 10
+
+GROUP BY C.F
+HAVING C.F > 8
+
+ORDER BY C.F ASC, C.D DESC --C.F기준 오름차순 후 C.D 기준 내림차순
+
+LIMIT 8 --상위 8개만 출력
